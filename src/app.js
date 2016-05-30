c0bc1fbf9cb7addb76a0c6a1312b1c1ae8f4dcd7
Samson Radu <radusamson@ymail.com>
2016-05-30 23:28:59 +0300

    formatting

--- src/app.js
+++ src/app.js
@@ -12,19 +12,19 @@ var path = require('path');
 app.use(express.static('html'));
 
 io.on('connection', function(socket){
-	socket.on('join', function(data){
-		let name = data.name;
-		console.log(name + ' joined');
-		var p = new Player(data.name, socket);
-		t.add(p);
-	});
-	socket.on('disconnect', function(){
-		t.remove(socket);
-	});
+    socket.on('join', function(data){
+        let name = data.name;
+        console.log(name + ' joined');
+        var p = new Player(data.name, socket);
+        t.add(p);
+    });
+    socket.on('disconnect', function(){
+        t.remove(socket);
+    });
 });
 
 http.listen(80, function(){
-	console.log('listening on *:80');
+    console.log('listening on *:80');
 });
 
 
--- src/components/player.js
+++ src/components/player.js
@@ -3,87 +3,87 @@
 let EventEmitter = require('events');
 
 class Player extends EventEmitter {
-	constructor(name, socket){
-		super();
+    constructor(name, socket){
+        super();
 
-		this.name = name;
+        this.name = name;
 
-		this.acceptFlag = null; //accept first 4 cards
+        this.acceptFlag = null; //accept first 4 cards
 
-		this.socket = socket;
+        this.socket = socket;
 
-		this.isCurrent = false;
+        this.isCurrent = false;
 
-		socket.on('play', (function(data){
-			if (data && data.length === 2)
-				this.take(data[0], data[1]);
-		}).bind(this));
+        socket.on('play', (function(data){
+            if (data && data.length === 2)
+                this.take(data[0], data[1]);
+        }).bind(this));
 
-		socket.on('confirm', (function(proposal){
-			this.confirm(proposal);
-		}).bind(this));
+        socket.on('confirm', (function(proposal){
+            this.confirm(proposal);
+        }).bind(this));
 
-		socket.on('message', (function(data){
-			this.message(data);
-		}).bind(this));
+        socket.on('message', (function(data){
+            this.message(data);
+        }).bind(this));
 
-		socket.on('reject', (function(proposal){
-			this.reject(proposal);
-		}).bind(this));
+        socket.on('reject', (function(proposal){
+            this.reject(proposal);
+        }).bind(this));
 
-		socket.on('accept', (function(){
-			this.accept();
-		}).bind(this));
+        socket.on('accept', (function(){
+            this.accept();
+        }).bind(this));
 
-		socket.on('refuse', (function(){
-			this.refuse();
-		}).bind(this));
-		
-		this.hand = [];
-		
-		this.stack = [];
+        socket.on('refuse', (function(){
+            this.refuse();
+        }).bind(this));
+        
+        this.hand = [];
+        
+        this.stack = [];
 
-		this.extraPoints = 0; //also known as "table"
-	}
+        this.extraPoints = 0; //also known as "table"
+    }
 
-	getPoints(){
-		return this.extraPoints + this.stack.reduce(function(prev, current, index){
-			let points = 0;
-			if (current === "2C" || current === "TD")
-				points = 2;
-			else if (["T", "J", "Q", "K", "A"].indexOf(current.charAt(0)) !== -1)  
-				points = 1;
+    getPoints(){
+        return this.extraPoints + this.stack.reduce(function(prev, current, index){
+            let points = 0;
+            if (current === "2C" || current === "TD")
+                points = 2;
+            else if (["T", "J", "Q", "K", "A"].indexOf(current.charAt(0)) !== -1)  
+                points = 1;
 
-			return prev + points;
-		}, 0);
-	}
+            return prev + points;
+        }, 0);
+    }
 
-	//take 0 or more table cards with a card from hand
-	take(card, tableCards){
-		this.emit('take', [card, tableCards]);
-	}
+    //take 0 or more table cards with a card from hand
+    take(card, tableCards){
+        this.emit('take', [card, tableCards]);
+    }
 
-	confirm(proposal){
-		this.emit('confirm', proposal);
-	}
+    confirm(proposal){
+        this.emit('confirm', proposal);
+    }
 
-	reject(proposal){
-		this.emit('reject', proposal);
-	}
+    reject(proposal){
+        this.emit('reject', proposal);
+    }
 
-	accept(){
-		this.acceptFlag = true;
-		this.emit('accept');
-	}
+    accept(){
+        this.acceptFlag = true;
+        this.emit('accept');
+    }
 
-	refuse(){
-		this.acceptFlag = false;
-		this.emit('refuse');
-	}
+    refuse(){
+        this.acceptFlag = false;
+        this.emit('refuse');
+    }
 
-	message(data){
-		this.emit('message', data);
-	}
+    message(data){
+        this.emit('message', data);
+    }
 }
 
 module.exports = Player;
